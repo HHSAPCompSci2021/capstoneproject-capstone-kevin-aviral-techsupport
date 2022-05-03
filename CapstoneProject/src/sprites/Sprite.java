66c418d7e7fc80b56b7490df5b59295eed467b3f
Aviral Vaidya <67848938+Aesteric@users.noreply.github.com>
2022-05-03 12:50:22 -0700

    skeleton

--- CapstoneProject/src/sprites/Sprite.java
+++ CapstoneProject/src/sprites/Sprite.java
@@ -1,5 +1,29 @@
 package sprites;
+import java.awt.*;
 
 public class Sprite {
+    private double x, y;
+    private double xv, yv;
+    private Color c;
+    private double ax, ay;
+    
+    public Sprite (double x, double y, double xv, double yv, Color c, double ax, double ay) {
+    	this.x = x;
+    	this.y = y;
+    	this.xv = xv;
+    	this.yv = yv;
+    	this.c = c;
+    	this.ax = ax;
+    	this.ay = ay;
+    }
+    
+    public void draw () {
+    	
+    }
+    
+    public boolean isTouching () {
+    	return false; 
+    }
+    
     
 }
