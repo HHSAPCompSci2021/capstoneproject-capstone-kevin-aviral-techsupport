fa1fe4895730d0eabe1f6a566343bd569be8c7b2
kren29 <kren849@student.fuhsd.org>
2022-05-04 09:22:32 -0700

    nothing special

--- CapstoneProject/src/sprites/Enemy.java
+++ CapstoneProject/src/sprites/Enemy.java
@@ -1,5 +1,23 @@
 package sprites;
 
-public class Enemy {
+import java.awt.*;
+
+public class Enemy extends Sprite {
+
+
+    public Enemy(double x, double y, double vx, double vy, Color c, double ax, double ay, int total) {
+        super(x, y, vx, vy, c, ax, ay, total);
+    }
+
+    public void shoot(double targetX, double targetY) {
+        // create new projectile with velocity in the direciton of target point
+
+    }
+
+    public void draw(Papplet p) {
+        super.draw(p);
+        p.fill(200, 100, 100);
+        
+    }
     
 }
--- CapstoneProject/src/sprites/Sprite.java
+++ CapstoneProject/src/sprites/Sprite.java
@@ -1,13 +1,16 @@
 package sprites;
 
 import java.awt.*;
+import processing.core.PApplet;
 
 public class Sprite {
+
     private double x, y;
     private double vx, vy;
     private Color c;
     private double ax, ay;
     private int lives, totalLives;
+    
 
     public Sprite(double x, double y, double vx, double vy, Color c, double ax, double ay, int totalLives) {
         this.x = x;
@@ -18,21 +21,40 @@ public class Sprite {
         this.ax = ax;
         this.ay = ay;
         this.totalLives = totalLives;
+        lives = totalLives;
     }
 
-    public void draw() {
+    public Sprite(double x, double y) {
+        this.x = x;
+        this.y = y;
+        c = new Color(100, 100, 100);
+        vx = 0;
+        vy = 0;
+        ax = 0;
+        ay = 0;
+        totalLives = 1;
+        lives = 1;
+    }
+    
+    public void draw(PApplet p) {
         x += vx;
         y += vy;
         vx += ax;
         vy += ay;
     }
 
-    public boolean isTouching() {
+    public boolean isTouching(Sprite other) {
         return false;
     }
 
     public void moveTo(double x2, double y2) {
-
-    }
+		x = x2;
+        y = y2;
+	}
+	
+	public void moveByAmount(double dx, double dy) {
+		x += dx;
+        y += dy;
+	}
 
 }
