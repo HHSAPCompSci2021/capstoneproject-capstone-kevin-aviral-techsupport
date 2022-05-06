e209279c26cdcca3e79c6cf6fb687b04f18542e3
Aviral Vaidya <67848938+Aesteric@users.noreply.github.com>
2022-05-06 12:49:32 -0700

    javadoc

--- CapstoneProject/src/screens/Screen.java
+++ CapstoneProject/src/screens/Screen.java
@@ -9,32 +9,48 @@ package screens;
 public abstract class Screen {
 
 	private final int DRAWING_WIDTH, DRAWING_HEIGHT;
-	
+	/**
+	 * creates a screen with a width and height
+	 * @param width width of the screen
+	 * @param height height of the screen
+	 */
 	public Screen(int width, int height) {
 		this.DRAWING_WIDTH = width;
 		this.DRAWING_HEIGHT = height;
 	}
-	
+	/**
+	 * sets up the window
+	 */
 	public void setup() {
 		
 	}
-	
+	/**
+	 * draws the screen
+	 */
 	public void draw() {
 		
 	}
-	
+	/**
+	 * detects client mouse input via clicks
+	 */
 	public void mousePressed() {
 		
 	}
-	
+	/**
+	 * detects client mouse input via mouse movement
+	 */
 	public void mouseMoved() {
 		
 	}
-	
+	/**
+	 * detects client mouse input via dragging
+	 */
 	public void mouseDragged() {
 		
 	}
-	
+	/**
+	 * detects client mouse input releases
+	 */
 	public void mouseReleased() {
 		
 	}
