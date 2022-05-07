4fa56de26c4a6f7fd77fb357cd26b209d15e85cf
Aviral Vaidya <67848938+Aesteric@users.noreply.github.com>
2022-05-06 17:05:25 -0700

    alpha (updated javadoc)

--- CapstoneProject/src/sprites/Platform.java
+++ CapstoneProject/src/sprites/Platform.java
@@ -4,16 +4,31 @@ import java.awt.*;
 import processing.core.*;
 import aviral.shapes.Shape;
 import aviral.shapes.Rectangle;
-
+/**
+ * The platform class represents a double precision platform that can be displayed with processing 
+ * @author Aviral
+ *
+ */
 public class Platform extends Sprite {
 
     // creates a platform at a random location
     // represented by a line
+	/**
+	 * creates a platform object at a random location
+	 * @param s shape of the platform
+	 * @param vx x velocity of platform
+	 * @param vy y velocity of platform
+	 */
     public Platform(Shape s, double vx, double vy) {
         super(s, vx, vy);
     }
 
-
+    /**
+     * draws the platform
+     * @param p surface to draw on
+     * @param player the player 
+     * @post platform is drawn on the surface
+     */
     public void draw(PApplet p, Player player) {
         super.draw(p);
         p.fill(255);
--- CapstoneProject/src/sprites/Player.java
+++ CapstoneProject/src/sprites/Player.java
@@ -3,9 +3,22 @@ package sprites;
 import aviral.shapes.Shape;
 import aviral.shapes.*;
 import java.awt.*;
-
+/**
+ * 
+ * @author Aviral Vaidya, Kevin Ren
+ * The Player class represents a double precision player that can be visualized using the processing library
+ *
+ */
 public class Player extends Sprite {
-	
+	/**
+	 * creates a player object
+	 * @param s shape of player
+	 * @param vx x velocity of player
+	 * @param vy y velocity of player
+	 * @param ax x acceleration of player
+	 * @param ay y acceleration of player
+	 * @param totalLives
+	 */
     public Player(Shape s, double vx, double vy, double ax, double ay, int totalLives) {
         super(s, vx, vy, ax, ay, totalLives);
     }
